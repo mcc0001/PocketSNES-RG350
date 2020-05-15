c907567d331ccd935e9baa9106dc8888c5ef41f5
ChiaYen Kan <kcygamil@gmail.com>
2020-05-15 10:44:31 +0800

    fix output name and search path error

--- opk/make_opk.sh
+++ opk/make_opk.sh
@@ -7,7 +7,7 @@ OPK_NAME=PocketSNES.opk
 echo Building ${OPK_NAME}...
 
 # create opk
-FLIST="../dist/PocketSNES.dge default.gcw0.desktop ../dist/backdrop.png sfc.png"
+FLIST="../pocketsnes/PocketSNES.dge default.gcw0.desktop ../pocketsnes/backdrop.png sfc.png"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
